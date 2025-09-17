b3fe4ca52ea2ecb08b5e317368422620fe127152
Bona.OQuinn <bonatoquinn@gmail.com>
2025-09-16 20:15:42 -0700

    Added math / display functions
    calc.js was updated with 4 new mathematical funcitons(add/sub/mult/division) which take in two parameters and returns the corresponding expressions. A operator expression was added as well, which works the same as the last 4 but has a third parameter which takes in an operator to perform on the num inputs. Lastly, a nodelist was created for the button on the face of the calculator to display whichever button was clicked on.

--- calc.js
+++ calc.js
@@ -7,29 +7,83 @@ let body = document.querySelector(".body");
 
 
 //function for addition 
-function add(a, b) {
-    return a + b; 
+function add(num1, num2) {
+    return num1 + num2; 
 
 }
 
 
-
 //function for subtraction 
-function sub(a, b) {
-    return a - b; 
+function sub(num1, num2) {
+    return num1 - num2; 
 }
 
 
 
 //function for multiplication 
-function mult(a, b) {
-    return a * b; 
+function mult(num1, num2) {
+    return num1 * num2; 
 }
 
 
 
 //function for division 
 
-function division(a, b) {
-    return a / b; 
+function division(num1, num2) {
+    return num1 / num2; 
 }
+
+
+
+//operator function 
+/* 
+3 parameters: 2 nums / operator symbol 
+returns num1 (op) num2
+*/
+
+function operator(op, num1, num2) {
+    switch (op) {
+        case "+":
+            return num1 + num2; 
+            break; 
+        case "-": 
+            return num1 - num2; 
+            break; 
+        case "*": 
+            return num1 * num2;
+            break; 
+        case "/": 
+            return num1 / num2; 
+            break;
+        default: 
+            return "INVALID"; 
+            break; 
+    }
+}
+
+
+/*
+populating calculator window with the corresponding button:
+
+psuedo:
+-create nodelist with all the buttons on calculator
+-access text value of each button and store in a variable 
+-create new elements to hold values of the button that was clicked
+-add values to element text content
+-add elements to head-items
+*/
+
+let bpush = document.querySelectorAll(".button");
+let displayCont = document.querySelector(".head-items"); 
+
+bpush.forEach(button => {
+    button.addEventListener("click", event => {
+        let text = event.target.textContent;
+
+        let display = document.createElement("div"); 
+        display.textContent = text; 
+        display.classList.toggle("textDisplay");
+        displayCont.appendChild(display);
+
+    })
+})
