16b13f11150b655da058b59e5ebc54866e4d1d28
Bona.OQuinn <bonatoquinn@gmail.com>
2025-09-16 18:54:45 -0700

    updated padding
    padding was added to the body container to fit visuals better

--- calc.js
+++ calc.js
@@ -6,3 +6,30 @@
 let body = document.querySelector(".body"); 
 
 
+//function for addition 
+function add(a, b) {
+    return a + b; 
+
+}
+
+
+
+//function for subtraction 
+function sub(a, b) {
+    return a - b; 
+}
+
+
+
+//function for multiplication 
+function mult(a, b) {
+    return a * b; 
+}
+
+
+
+//function for division 
+
+function division(a, b) {
+    return a / b; 
+}
